d777a944f33cbb853d4f599e8302cb8cbb8e98b5
Brittany Brimley <brittanysapp@gmail.com>
2023-12-30 23:03:25 -0700

    created Set model

---
+++ server/models/Set.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+// create our sets model
+class Sets extends Model { }
+
+// create fields/columns for sets model
+Sets.init(
+    {
+        set_id: {
+            allowNull: false,
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        set_name: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        parts: {
+            type: DataTypes.INTEGER
+        },
+        label_id: {
+            allowNull: false,
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'labels',
+                key: 'label_id',
+            },
+        },
+        user_id: {
+            allowNull: false,
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        // Prevent sequelize from renaming the table
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'sets'
+    }
+);
+
+module.exports = Sets;
