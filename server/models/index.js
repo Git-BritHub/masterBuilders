0f1392af85a332534c4f8737f4d6d6b39615c9ce
Brittany Brimley <brittanysapp@gmail.com>
2024-01-01 21:36:52 -0700

    created index file for models

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Category = require('./Category');
+const Collection = require('./Collection');
+const Wishlist = require('./Wishlist');
+const Sets = require('./Sets');
+
+module.exports = { User, Category, Collection, Wishlist, Sets };
