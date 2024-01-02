993e83411e9f71c31acb0604241bdd484dc0e94e
Brittany Brimley <brittanysapp@gmail.com>
2024-01-01 22:14:57 -0700

    created resolvers and typeDef files, will finish once Post, Comment and React schemas are added to models

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+const { User, Category, Collection, Wishlist, Sets, Post } = require('../models');
+// const {
+// 	signToken,
+// 	AuthenticationError,
+// } = require('../utils/auth');
+
+const resolvers = {
+
+}
+
+module.exports = resolvers;
