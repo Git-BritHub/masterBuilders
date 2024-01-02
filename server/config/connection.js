095b7d43103b6cbdab5784130895a882ebf7f3e8
Brittany Brimley <brittanysapp@gmail.com>
2024-01-01 23:22:36 -0700

    set up temp production/testing database connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
-require(dotenv);
-const mongoose = require("mongoose");
+require('dotenv');
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/masterBuilders');
 
 module.exports = mongoose.connection;
